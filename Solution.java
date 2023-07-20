a5bb912bece2eae8010f4bebb1827959e2489d40
Ankush Gaur <MY_NAME@example.com>
2023-07-20 15:32:38 +0530

    Asteroid Collision Day#-19

---
+++ Solution.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+
+public class Solution {
+    public int[] astroidcollision(int[] asteroids){
+        LinkedList<Integer> list=new LinkedList<>();
+        for(int i : asteroids){
+            while(!list.isEmpty() && list.getLast() > 0 && list.getLast() < -i)
+            list.pollLast();
+            if(list.isEmpty()|| i > 0 || list.getLast() < -0)
+            list.add(i);
+            else if(i < 0 && list.getLast() == -i)
+            list.pollLast();
+        }  
+        return list.stream().mapToInt(i->i).toArray();
+
+
+    }
+
+}
